e99c115386bcd772b4f8255b09b625920ae882ec
abdulvadudraximjonov-dev <abdulvadudraximjonov@gmail.com>
2026-07-15 18:36:55 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+TOKEN="8914458372:AAGPtSPLyR-1im-imn6lhPD9ZR4K9gvhBHE"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "🎌 Salom! Anime botga xush kelibsiz.\n\n"
+        "/anime - Anime tavsiyasi"
+    )
+
+async def anime(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "🍿 Tavsiya:\n"
+        "• Naruto\n"
+        "• One Piece\n"
+        "• Attack on Titan\n"
+        "• Demon Slayer\n"
+        "• Jujutsu Kaisen"
+    )
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("anime", anime))
+
+app.run_polling()
